a805b8ba406ed1256f5a8a5d62c837679f4fdd45
ludanyitamas9 <ludanyitamas9@gmail.com>
2019-06-08 23:30:45 +0200

    DB_kapcsolat_prepar_select

--- src/KAPCSOLAT.java
+++ src/KAPCSOLAT.java
@@ -27,25 +27,46 @@ public class KAPCSOLAT {
       public static   String select_EUSZOLG_ENGEDELY = ("SELECT distinct cASE WHEN  count(TORZSADAT.e$euszolg_engedely.bejegyzes_dt) = 0\n" +
 "THEN 'Aktív'\n" +
 "Else 'Nem aktív'\n" +
-"END AS SZINKRON\n" +
+"END AS valami\n" +
 "FROM  TORZSADAT.e$euszolg_engedely WHERE SUBSTR(bejegyzes_dt,1,10) >= TRUNC(SYSDATE-1) AND SUBSTR(bejegyzes_dt,1,10) < TRUNC(SYSDATE)");
+      
+      public static String select_EUSZOLG = ("SELECT distinct cASE WHEN  count(TORZSADAT.e$euszolg.bejegyzes_dt) = 0\n" +
+"THEN 'Aktív'\n" +
+"Else 'Nem aktív'\n" +
+"END AS SZINKRON\n" +
+"FROM  TORZSADAT.e$euszolg WHERE SUBSTR(bejegyzes_dt,1,10) >= TRUNC(SYSDATE-1) AND SUBSTR(bejegyzes_dt,1,10) < TRUNC(SYSDATE)");
+      public static String select_EUSZOLG_KOZREMUKODO = ("SELECT distinct cASE WHEN  count(TORZSADAT.e$euszolg_kozremukodo.bejegyzes_dt) = 0\n" +
+"THEN 'Aktív'\n" +
+"Else 'Nem aktív'\n" +
+"END AS SZINKRON\n" +
+"FROM  TORZSADAT.e$euszolg_kozremukodo WHERE SUBSTR(bejegyzes_dt,1,10) >= TRUNC(SYSDATE-1) AND SUBSTR(bejegyzes_dt,1,10) < TRUNC(SYSDATE)");
+      
+              
+    @SuppressWarnings("empty-statement")
     public static void main(String[] args) throws ClassNotFoundException {
          
             
-          upsz          
-        
+         
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
             Connection con = DriverManager.getConnection(
                     "jdbc:oracle:thin:@localhost:23830:CORE1", "ludanyitamas", "Skorpio95@");
             
                               preparedStatement = con.prepareStatement(select_alapnyil);
-                              //preparedStatement = con.prepareStatement(select_EUSZOLG_ENGEDELY);
-                              rs = preparedStatement.executeQuery(select_alapnyil);;
-                              //rs = preparedStatement.executeQuery(select_EUSZOLG_ENGEDELY);
-                    while (rs.next()) {
-                        System.out.println(rs.getString(1) );
+                              rs = preparedStatement.executeQuery(select_alapnyil);
+                              while (rs.next()) {
+                        System.out.println("Alapnyilvántartás: "+rs.getString(1));
                     }
+                              preparedStatement = con.prepareStatement(select_EUSZOLG_ENGEDELY);
+                              rs = preparedStatement.executeQuery(select_EUSZOLG_ENGEDELY);
+                              while (rs.next()) {
+                        System.out.println("EUSZOLG_Engedély: "+rs.getString(1));
+                    }
+                         
+ 
+                              
+                              
+                    
 
             //step5 close the connection object  
             con.close();
